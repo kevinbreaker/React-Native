f5ace877d51a189e3655a1dca76e654e7606afd8
Kevin Martin <kevinbreaker2604@gmail.com>
2017-12-03 05:12:54 -0300

    Finalizando..

--- GitHubApp/src/pages/organizations/components/organization/index.js
+++ GitHubApp/src/pages/organizations/components/organization/index.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
 import { Text, View, Image } from 'react-native';
+import PropTypes from 'prop-types';
 import styles from './style';
 
+
 export default class Organization extends Component {
+  static propTypes = {
+    organization: PropTypes.shape({
+      avatar_url: PropTypes.string,
+      login: PropTypes.string,
+    }).isRequired,
+  }
+
   render() {
+    const { organization } = this.props;
+
     return (
       <View style={styles.container}>
         <Image
           style={styles.avatar}
-          source={{ uri: 'https://avatars1.githubusercontent.com/u/25128546?s=200&v=4' }}
+          source={{ uri: organization.avatar_url }}
         />
-        <Text style={styles.title}>React-Native</Text>
+        <Text style={styles.title}>{organization.login}</Text>
       </View>
     );
   }
--- GitHubApp/src/pages/organizations/index.js
+++ GitHubApp/src/pages/organizations/index.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
-import { ScrollView } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import api from 'services/api';
+
+import {
+  ScrollView,
+  Text,
+  AsyncStorage,
+  ActivityIndicator,
+} from 'react-native';
 
 import Organization from './components/organization';
 
@@ -12,16 +19,46 @@ export default class Organizations extends Component {
       <Icon name="building" size={20} color={tintColor} />
     ),
   }
+
+  state = {
+    organizations: [],
+    loading: false,
+  };
+
+  componentWillMount() {
+    this.setState({ loading: true });
+
+    this.loadOrganizations()
+      .then(() => this.setState({ loading: false }));
+  }
+
+  loadOrganizations = async () => {
+    const username = await AsyncStorage.getItem('@GithubApp:username');
+    const response = await api.get(`users/${username}/orgs`);
+
+    this.setState({ organizations: response.data });
+  };
+
+  renderOrganizations = () => (
+    this.state.organizations.map(organization => (
+      <Organization key={organization.id} organization={organization} />
+    ))
+  );
+
+  renderList = () => (
+    this.state.organizations.length
+      ? this.renderOrganizations()
+      : <Text style={styles.empty}>Nenhuma organização encontrada</Text>
+  );
   render() {
     return (
       <ScrollView
         style={styles.container}
         contentContainerStyle={styles.contentContainer}
       >
-        <Organization />
-        <Organization />
-        <Organization />
-        <Organization />
+        { this.state.loading
+          ? <ActivityIndicator size="small" color="#999" style={styles.loading} />
+          : this.renderList() }
       </ScrollView>
     );
   }
